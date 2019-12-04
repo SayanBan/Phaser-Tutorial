c2a197df8b5d8ae1e8bff3c0bf7b6c9016ecb84a
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-12-04 18:23:17 +0530

    Create game.js

---
+++ Day 1/Part07/game.js
@@ -0,0 +1,27 @@
+let codey;
+
+function preload() {
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+}
+
+function create() {
+  codey = this.add.sprite(30, 200, 'codey')
+}
+
+// Create your update() function here
+function update(){
+codey.x += 1;  
+}
+const config = {
+	type: Phaser.AUTO,
+	width: 400,
+	height: 400,
+	backgroundColor: "#5f2a55",
+	scene: {
+    preload,
+    create,
+    update,
+	}
+}
+
+const game = new Phaser.Game(config)
